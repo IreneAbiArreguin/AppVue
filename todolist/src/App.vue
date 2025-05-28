810f6a9f798337b04c992d89d8fca17a18f96370
EstudianteMartinez <l21390264@chetumal.tecnm.mx>
2025-05-28 17:31:38 -0500

    Actualizar

---
+++ todolist/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppHeader from "./components/AppHeader.vue";
+import TodoList from "./components/TodoList.vue";
+</script>
+
+<template>
+  <AppHeader />
+
+  <TodoList />
+</template>
+
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f4f4f4;
+}
+header {
+  background-color: #4caf50;
+  color: white;
+  padding: 10px 0;
+  text-align: center;
+}
+</style>
